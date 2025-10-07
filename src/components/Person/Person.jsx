c8b8191c67a0b4e33ca66f9660cc6eb1c5e13224
Denys Anokhin <denys.anokhin.dev@gmail.com>
2025-10-07 23:18:29 +0300

    resolve 3

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -1,15 +1,23 @@
 export const Person = ({ person }) => {
-  const partnerLabel = person.isMarried
-    ? (person.sex === 'm'
-      ? `${person.partnerName} is my wife`
-      : `${person.partnerName} is my husband`)
-    : 'I am not married';
+  const { name, age, isMarried, sex, partnerName } = person;
+
+  let partnerLabel = 'I am not married';
+
+  if (isMarried) {
+    if (sex === 'm') {
+      partnerLabel = `${partnerName} is my wife`;
+    } else {
+      partnerLabel = `${partnerName} is my husband`;
+    }
+  }
 
   return (
     <section className="Person">
-      <h2 className="Person__name">My name is { person.name }</h2>
-      { person.age && ( <p className="Person__age">I am { person.age }</p> ) }
-      <p className="Person__partner">{ partnerLabel }</p>
+      <h2 className="Person__name">My name is {name}</h2>
+      {age && <p className="Person__age">I am {age}</p>}
+      {partnerLabel && (
+        <p className="Person__partner">{partnerLabel}</p>
+      )}
     </section>
   );
 }
